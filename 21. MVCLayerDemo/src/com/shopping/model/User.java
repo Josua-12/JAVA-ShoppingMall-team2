889f479de2e30b5cc18f28652b4b901e4c8b9510
jsua86268 <jsua86268@gmail.com>
2025-08-08 10:20:03 +0900

    수아: Person, User  클래스 수정

--- 21. MVCLayerDemo/src/com/shopping/model/User.java
+++ 21. MVCLayerDemo/src/com/shopping/model/User.java
@@ -7,44 +7,20 @@ import java.io.Serializable;
  * Serializable를 구현하여 파일 저장이 가능하도록 함 
  */
 
-public class User implements Serializable {
+public class User extends Person implements Serializable {
 	
 	//직렬화 버전 UID (파일 저장/읽기 시 클래스 버전 관리)
 	private static final long serialVersionID = 1L;
 	
-	//필드
-	private String id;	//사용자 ID 
-	private String password;
-	private String name;
 	private double balance;
 	
-	public User(String id, String password, String name) {
-		//super();
-		this.id = id;
-		this.password = password;
-		this.name = name;
-		this.balance = 10000.0;		//초기 잔액 설정
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public String getName() {
-		return name;
+	public User(String id, String password, String email, String name) {
+		super(id, password, email, name);	// 부모(person)생성자 호출
+		this.balance = 10000.0;	// 초기 잔액
 	}
 
 	public double getBalance() {
 		return balance;
-	}
-
-	public void setPassword(String password) {
-		if(password != null && !password.isEmpty())
-			this.password = password;
 	}
 
 	public void setBalance(double balance) {
@@ -54,8 +30,11 @@ public class User implements Serializable {
 			System.err.println("잔액은 음수가 될 수 없습니다.");
 		}
 	}
-	
-	
+
+	@Override
+	public String getRole() {
+		return "회원";
+	}	
 }
 
 
